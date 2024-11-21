9cfef9b3626c02d5c18ed16b6815a8d474023f38
Skufu <agpfrancisco1@gmail.com>
2024-11-21 16:45:47 +0800

    player.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    pygame.init
+    pygame.init()
+    player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH }")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     while True:
-        screen.fill('black')
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return
-        
-    
-
+            
+        screen.fill('black')
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
