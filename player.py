47079880c9a64cb99f1a8a08436a6084768c0f86
Skufu <agpfrancisco1@gmail.com>
2024-12-01 18:32:37 +0800

    added bullet

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 from circleshape import CircleShape
 import pygame
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0.0
         
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -31,9 +33,19 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            if self.timer <= 0:
+                self.shoot()
+                self.timer = PLAYER_SHOT_COOLDOWN
+        self.timer -= dt if self.timer > 0 else 0
             
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    
+    def shoot(self):
+        velocity = pygame.Vector2(0,1).rotate(self.rotation)
+        velocity = velocity * PLAYER_SHOOT_SPEED
         
+        Shot(self.position.x, self.position.y, velocity)
         
--- shot.py
+++ shot.py
@@ -1,15 +1,16 @@
+from circleshape import CircleShape
 import pygame
-from circleshape import *
+from constants import *
 
-class Asteroid(CircleShape):
-    def __init__(self,x,y,radius):
-        super().__init__(x,y,radius)
-    
+class Shot(CircleShape):
+    def __init__(self,x,y, velocity):
+        super().__init__(x,y,SHOT_RADIUS)
+        self.velocity = velocity
+        
     def draw(self,screen):
         pygame.draw.circle(screen, (255,255,255), (self.position.x, self.position.y), self.radius, 1)
     
     def update(self, dt):
         self.position += self.velocity * dt
-                
-
+     
         
